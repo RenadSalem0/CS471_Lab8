3eb266525c12618d45b56cf30d194ca93d6c2e20
RenadSalem0 <Renad9789@gmail.com>
2026-04-13 10:19:01 +0300

    Add files via upload

---
+++ DjangoProjects/apps/bookmodule/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    price = models.FloatField(default=0.0)
+    edition = models.SmallIntegerField(default=1)
+
+class Address(models.Model):
+    city = models.CharField(max_length=100)
+
+    def student_count(self):
+        return Student.objects.filter(address=self).count()
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
